675d48764587d0b7895ae8f51f4ce73bfd8f9512
Sahril <namakusahril@gmail.com>
2024-11-04 21:48:57 +0700

    Refactor SplashView layout to simplify structure and enhance gradient background

--- lib/app/modules/splash/views/splash_view.dart
+++ lib/app/modules/splash/views/splash_view.dart
@@ -12,35 +12,31 @@ class SplashView extends GetView<SplashController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFF439A31),
+              Color(0xFF0868AE),
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+          child: Container(
+            padding: EdgeInsets.all(20),
             decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Color(0xFF439A31), // Warna pertama (#439A31)
-                  Color(0xFF0868AE), // Warna kedua (#0868AE)
-                ],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-              ),
+              color: Colors.white,
+              shape: BoxShape.circle,
             ),
-            child: Center(
-              child: Container(
-                padding: EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  shape: BoxShape.circle,
-                ),
-                child: Image.asset(
-                  Assets.images.logo.path,
-                  width: 50.w,
-                  height: 150.h,
-                ),
-              ),
+            child: Image.asset(
+              Assets.images.logo.path,
+              width: 50.w,
+              height: 150.h,
             ),
           ),
-        ],
+        ),
       ),
     );
   }
